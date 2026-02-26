4f8f0c507f2d8633532f6d3930b87c613c8ee86b
JPAM937alt <104039015+JPAM937alt@users.noreply.github.com>
2026-02-26 02:50:13 +0000

    Update Tools.java
    Signed-off-by: JPAM937alt <104039015+JPAM937alt@users.noreply.github.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -1698,9 +1698,9 @@ public final class Tools {
     }
 
     public static boolean isLocalProfile(Context ctx){
-        MinecraftAccount currentProfile = PojavProfile.getCurrentProfileContent(ctx, null);
-        return currentProfile == null || currentProfile.isLocal();
+        return false;
     }
+    
     public static boolean hasOnlineProfile(){
         return true;
     }
